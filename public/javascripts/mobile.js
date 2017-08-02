bfe9130db1d53aa2b709d94ee0ef313322254230
Unknown <dilcia_salgado1995@hotmail.com>
2017-08-01 19:14:51 -0600

    Hasta aqui

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -14,3 +14,36 @@ $("#splash").on('pagecreate',function(e){
     'json'
   );
 });
+
+
+$("#itemCreate").on('pagecreate', function(e){
+  $("#itemCreate-enviar").on('click',function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var formObject={};
+    formObject.nombrepelicula = $("#itemCreate-txtNombrePelicula").val();
+    formObject.numerosala = $("#itemCreate-txtNombre").val();
+    formObject.fechafuncion = $("#itemCreate-dateFuncion").val();
+    formObject.hora = $("#itemCreate-txtHora").val();
+    formObject.formatopelicula = $("#itemCreate-txtFormato").val();
+    formObject.Clasificacion = $("#itemCreate-txtClasificacion").val();
+    formObject.nombrecompleto = $("#itemCreate-txtNombre").val();
+    formObject.correo = $("#itemCreate-txtCorreo").val();
+    formObject.Asientos = $("#itemCreate-txtAsientos").val();
+
+
+    $.post("/api/createitem",
+    formObject,
+    function(response,txtscs,xhrq){
+      if(response){
+        alert('Documento isertado');
+      }
+    },
+    'json'
+  );
+    console.log(formObject);
+
+
+
+  });
+});
--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,38 @@ function iniApiRoute(db){
   var reserva= db.collection('reserva');
 
   router.get('/getLista',function(req,res,next){
-    reserva.find({}).limit(4).toArray(function(err,docs){
+    reserva.find({}).toArray(function(err,docs){
       if(err){
         res.status(500).json({"error":"No se pudieron obtener las reservas"});
       }else{
         res.status(200).json(docs);
       }
     })
+  });
+
+  router.post('/createitem',function(req,res,next){
+      var doc = {nombrepelicula:"",numerosala:0, fechafuncion:"", hora:"", formatopelicula:"", Clasificacion:"", nombrecompleto:"",correo:"", Asientos: 0};
+      doc.nombrepelicula =req.body.nombrepelicula;
+      doc.numerosala =req.body.numerosala;
+      doc.fechafuncion =req.body.fechafuncion;
+      doc.hora =req.body.hora;
+      doc.formatopelicula =req.body.formatopelicula;
+      doc.Clasificacion =req.body.Clasificacion;
+      doc.nombrecompleto =req.body.nombrecompleto;
+      doc.correo =req.body.correo;
+      doc.Asientos =req.body.Asientos;
+
+
+
+
+      reserva.insertOne(doc,function (err,result){
+        if(err){
+          res.status(500).json({"error": "Ocurrio un error"});
+
+        }else{
+            res.status(200).json({"insert":"ok"});
+        }
+      });
   });
   return router;
 };
